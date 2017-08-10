136603036854c32701e3474a86f4bfe166e590b9
Alexandre Breda Neves <bredaneves@gmail.com>
2017-08-09 22:05:02 -0300

    até aula 99

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 /*o parâmetro extended siginifica que mais informações além do padrão
 poderão ser recebidas na requisição do formulário*/
 /*abaixo estão as chamadas de middleware na ordem em que devem ser
@@ -11,6 +12,7 @@ interpretadas, primeiro como url e depois como json*/
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`)
